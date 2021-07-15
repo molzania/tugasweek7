ad8750913b506fa663e4bd9ce3e0a4b7b96bb875
molzania <molzania_1507@yahoo.co.id>
2021-07-15 15:35:05 +0700

    commit on 'main'

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Jawaban Nomor 1//
+//Jawaban Soal Nomor 1//
 //Buatlah data array yang berisi minimal 5 pekerjaan dibidang IT (Ex. Front end developer). 
 //Tampilkan panjang data Array
 //Tampilkan seluruh item pada array
@@ -11,10 +11,17 @@ let data = ['Software Developer', "Database Administrator", "Hardware Engineer",
 console.log("Jumlah Pekerjaan IT : " + data.length);
 console.log("Nama Pekerjaan IT : " + data);
 
+//Jawaban Soal Nomor 2//
+//Buatlah sebuah data array yang berisi nama pada tim teman-teman.
+//Bagi tim menjadi 2 tim dan terdiri dari 1 orang leader
+//Console.log data array tersebut dengan memisahkan setiap data menggunakan  koma (,)
+//Tampilkan list data tersebut ke halaman HTML
 
-let list = document.getElementById("myList");
-data.forEach((item) => {
-    let li = document.createElement("li");
-    li.innerText = item;
-    list.appendChild(li);
-  });
+let allMember=["Aldi","Bagas","Molly","Rodhiyah"];
+const team1=allMember.slice(0,2);
+const team2=allMember.slice(2,4);
+
+console.log("Tim", team1, team2);
+
+
+
