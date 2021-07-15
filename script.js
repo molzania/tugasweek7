ef79e70c9ab8c1ea0fb3e9c3bb5764619254ab8b
molzania <molzania_1507@yahoo.co.id>
2021-07-15 14:40:41 +0700

    commit on 'main'

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//
+//Jawaban Nomor 1//
+//Buatlah data array yang berisi minimal 5 pekerjaan dibidang IT (Ex. Front end developer). 
+//Tampilkan panjang data Array
+//Tampilkan seluruh item pada array
+//Gunakan Console.log
+//Tampilkan data tersebut pada Halaman HTML menggunakan list tag
+
+let pekerjaanIT = ['Software Developer', "Database Administrator", "Hardware Engineer", 
+"System Analyst", "Network Architect" ];
+ 
+console.log("Jumlah Pekerjaan IT : " + pekerjaanIT.length);
+console.log("Nama Pekerjaan IT : " + pekerjaanIT);
+
+document.getElementById("pekerjaanITLength").innerHTML = pekerjaanIT.length;
+document.getElementById("pekerjaanITData").innerHTML = pekerjaanIT.length;
