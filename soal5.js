b391343ac4cb535f6b04ccfb8a04817514a31c2c
molzania <molzania_1507@yahoo.co.id>
2021-07-22 12:57:49 +0700

    commit on 'main'

--- soal5.js
+++ soal5.js
@@ -6,3 +6,26 @@
 //tweet.push(tweet)
 //for(let i = 0; i < tweet.length; i++){
 //document.write(tweet[i])}
+
+// data array tweet
+let tweets = ["hello world", "tess tweet", "ini tweet"];
+
+// ambil elemen utk wadah tweet
+let tweetContainer = document.getElementById("tweet");
+
+// utk nampilin
+for (let tweet of tweets) {
+  console.log(tweet);
+  tweetContainer.innerHTML += `<p>${tweet}</p>`;
+}
+
+// ambil elemen text input dan button
+let txtInput = document.getElementById("text")
+let btnKirim = document.getElementById("btn-kirim")
+
+// fungsionalitas utk tombol kirim
+btnKirim.onclick = kirim
+function kirim() {
+  let text = txtInput.value
+  console.log(text);
+}
